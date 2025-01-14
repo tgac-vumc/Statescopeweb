568f8c0ee9d07365852f44f856f1f5f3c4991ce4
AryamaanBose <55136753+aryamaanbose@users.noreply.github.com>
2025-01-14 12:31:25 +0100

    Deploy website - based on d3f2b6af6676a980d1badcbbb2c98b13e1ee6263

---
+++ assets/js/6bc67261.d936f47a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkStatescope=self.webpackChunkStatescope||[]).push([[6008],{7644:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Statescopeweb/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
